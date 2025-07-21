1b2fcb17c9fb4a7236d966c721f58dd20daf50ab
EpicAryan <adaryan0811@gmail.com>
2025-07-21 19:15:07 +0530

    Initial Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center text-center text-5xl text-red-500'
+    >HomePage
+      <Button
+      variant='default'
+      size='lg'
+      >How</Button>
+    </div>
+  )
+}
+
+export default HomePage
